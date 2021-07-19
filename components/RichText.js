367eac516352688ad10a63e970b5b12c3521e9f9
Wei Zhang <wei@novvum.io>
2021-07-18 22:44:09 -0700

    updated rich text to pre-wrap

--- components/RichText.js
+++ components/RichText.js
@@ -1,10 +1,19 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { Flex } from "@chakra-ui/react";
+import { BLOCKS, MARKS } from "@contentful/rich-text-types";
+import { Flex, Text } from "@chakra-ui/react";
 
 const RichText = ({ text }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => (
+        <Text whiteSpace="pre-wrap">{children}</Text>
+      ),
+    },
+  };
+
   return (
     <Flex justify="center" direction="column" px="5em">
-      {documentToReactComponents(text)}
+      {documentToReactComponents(text, options)}
     </Flex>
   );
 };
